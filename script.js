03aeec28949dc00d04002960d08ca6069f5ed448
Michaelmw17 <Michaelmw17@outlook.com>
2020-09-21 13:00:00 +1000

    html markup

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //define the varibles that we'll need to append data pulled from api to cards above---------------------------------//
 var APIkey = "&appid=79665c3a613bbe39f50d60745b2798c6";
-var cityName = "Seattle";
+var cityName = "Sydney";
 var queryURL =
   "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=ecc0be5fd92206da3aa90cc41c13ca56";
 
